5ff927cb296722ada3ab2894d93bc83e69c298f0
Amitha Mohan <damitha.mohan@gmail.com>
2020-02-25 00:38:44 +0530

    Visakh - changes for webp - [VV]

--- src/js/app.js
+++ src/js/app.js
@@ -4,17 +4,22 @@ $('#carousel-example').on('slide.bs.carousel', function (e) {
     */
     var $e = $(e.relatedTarget);
     var idx = $e.index();
-    var itemsPerSlide = 5;
+    var itemsPerSlide = 4;
     var totalItems = $('.carousel-item').length;
  
-    if (idx >= totalItems-(itemsPerSlide-1)) {
+    if (idx >= totalItems-(itemsPerSlide-1)) 
+    {
         var it = itemsPerSlide - (totalItems - idx);
-        for (var i=0; i<it; i++) {
+
+        for (var i=0; i<it; i++) 
+        {
             // append slides to end
-            if (e.direction=="left") {
+            if (e.direction=="left") 
+            {
                 $('.carousel-item').eq(i).appendTo('.carousel-inner');
             }
-            else {
+            else 
+            {
                 $('.carousel-item').eq(0).appendTo('.carousel-inner');
             }
         }
